9f9fadf40222c8afb0f45e4670458bcefad2afb8
lyhe18 <lyhe2018@icloud.com>
2020-08-17 23:55:23 -0700

    Initialize typeRegistry map and fix DeliverBlock

--- abcix/adapter/adapter.go
+++ abcix/adapter/adapter.go
@@ -22,10 +22,18 @@ func (app *adaptedApp) OriginalApp() abci.Application {
 	return app.abciApp
 }
 
-var typeRegistry = make(map[string]reflect.Type)
-
-func registerType(s string, i interface{}) {
-	typeRegistry[s] = reflect.TypeOf(i)
+var typeRegistry = map[string]reflect.Type{
+	"abci.RequestInfo":               reflect.TypeOf(abci.RequestInfo{}),
+	"abci.RequestSetOption":          reflect.TypeOf(abci.RequestSetOption{}),
+	"abci.RequestQuery":              reflect.TypeOf(abci.RequestQuery{}),
+	"abci.RequestCheckTx":            reflect.TypeOf(abci.RequestCheckTx{}),
+	"abci.RequestInitChain":          reflect.TypeOf(abci.RequestInitChain{}),
+	"abci.RequestListSnapshots":      reflect.TypeOf(abci.RequestListSnapshots{}),
+	"abci.RequestOfferSnapshot":      reflect.TypeOf(abci.RequestOfferSnapshot{}),
+	"abci.RequestLoadSnapshotChunk":  reflect.TypeOf(abci.RequestOfferSnapshot{}),
+	"abci.RequestApplySnapshotChunk": reflect.TypeOf(abci.RequestApplySnapshotChunk{}),
+	"abci.RequestEndBlock":           reflect.TypeOf(abci.RequestEndBlock{}),
+	"abci.RequestBeginBlock":         reflect.TypeOf(abci.RequestBeginBlock{}),
 }
 
 func newStruct(name string) (interface{}, bool) {
@@ -65,34 +73,22 @@ func adaptGeneralization(req interface{}, resp interface{}, abciReq string, f in
 }
 
 func (app *adaptedApp) Info(req abcix.RequestInfo) (resp abcix.ResponseInfo) {
-	registerType("abci.RequestInfo", abci.RequestInfo{})
 	adaptGeneralization(&req, &resp, "abci.RequestInfo", app.abciApp.Info)
 	return
 }
 
 func (app *adaptedApp) SetOption(req abcix.RequestSetOption) (resp abcix.ResponseSetOption) {
-	registerType("abci.RequestSetOption", abci.RequestSetOption{})
 	adaptGeneralization(&req, &resp, "abci.RequestSetOption", app.abciApp.SetOption)
 	return
 }
 
 func (app *adaptedApp) Query(req abcix.RequestQuery) (resp abcix.ResponseQuery) {
-	registerType("abci.RequestQuery", abci.RequestQuery{})
 	adaptGeneralization(&req, &resp, "abci.RequestQuery", app.abciApp.Query)
 	return
 }
 
 func (app *adaptedApp) CheckTx(req abcix.RequestCheckTx) (resp abcix.ResponseCheckTx) {
-	abciReq := abci.RequestCheckTx{}
-	if err := copier.Copy(&abciReq, &req); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
-	abciResp := app.abciApp.CheckTx(abciReq)
-	if err := copier.Copy(&resp, &abciResp); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
+	adaptGeneralization(&req, &resp, "abci.RequestCheckTx", app.abciApp.CheckTx)
 	return
 }
 
@@ -102,23 +98,13 @@ func (app *adaptedApp) CreateBlock(req abcix.RequestCreateBlock, iter *abcix.Mem
 }
 
 func (app *adaptedApp) InitChain(req abcix.RequestInitChain) (resp abcix.ResponseInitChain) {
-	registerType("abci.RequestInitChain", abci.RequestInitChain{})
 	adaptGeneralization(&req, &resp, "abci.RequestInitChain", app.abciApp.InitChain)
 	return
 }
 
 func (app *adaptedApp) DeliverBlock(req abcix.RequestDeliverBlock) (resp abcix.ResponseDeliverBlock) {
-	var reqBegin abci.RequestBeginBlock
-	if err := copier.Copy(&reqBegin, &req); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
-	respBegin := app.abciApp.BeginBlock(reqBegin)
-	events := respBegin.Events
-	if err := copier.Copy(&resp, &respBegin); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
+	adaptGeneralization(&req, &resp, "abci.RequestBeginBlock", app.abciApp.BeginBlock)
+	beginEvents := resp.Events
 
 	for _, tx := range req.Txs {
 		legacyRespDeliverTx := app.abciApp.DeliverTx(abci.RequestDeliverTx{Tx: tx})
@@ -130,30 +116,12 @@ func (app *adaptedApp) DeliverBlock(req abcix.RequestDeliverBlock) (resp abcix.R
 		resp.DeliverTxs = append(resp.DeliverTxs, &respDeliverTx)
 	}
 
-	var reqEnd abci.RequestEndBlock
-	if err := copier.Copy(&reqEnd, &req); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
-	respEnd := app.abciApp.EndBlock(reqEnd)
-	events = append(events, respEnd.Events...)
-	if err := copier.Copy(&resp, &respEnd); err != nil {
-		// TODO: panic for debugging purposes. better error handling soon!
-		panic(err)
-	}
+	adaptGeneralization(&req, &resp, "abci.RequestEndBlock", app.abciApp.EndBlock)
+	endEvents := resp.Events
 
-	// Reform the events
-	resp.Events = make([]abcix.Event, len(events))
-	for i, oldEvent := range events {
-		oldEvent := oldEvent
-		var newEvent abcix.Event
-		if err := copier.Copy(&newEvent, &oldEvent); err != nil {
-			// TODO: panic for debugging purposes. better error handling soon!
-			panic(err)
-		}
-		resp.Events[i] = newEvent
-	}
-
+	resp.Events = make([]abcix.Event, len(beginEvents)+len(endEvents))
+	copy(resp.Events, beginEvents)
+	resp.Events = append(resp.Events, endEvents...)
 	return resp
 }
 
@@ -172,25 +140,21 @@ func (app *adaptedApp) CheckBlock(req abcix.RequestCheckBlock) abcix.ResponseChe
 }
 
 func (app *adaptedApp) ListSnapshots(req abcix.RequestListSnapshots) (resp abcix.ResponseListSnapshots) {
-	registerType("abci.RequestListSnapshots", abci.RequestListSnapshots{})
 	adaptGeneralization(&req, &resp, "abci.RequestListSnapshots", app.abciApp.ListSnapshots)
 	return
 }
 
 func (app *adaptedApp) OfferSnapshot(req abcix.RequestOfferSnapshot) (resp abcix.ResponseOfferSnapshot) {
-	registerType("abci.RequestOfferSnapshot", abci.RequestOfferSnapshot{})
 	adaptGeneralization(&req, &resp, "abci.RequestOfferSnapshot", app.abciApp.OfferSnapshot)
 	return
 }
 
 func (app *adaptedApp) LoadSnapshotChunk(req abcix.RequestLoadSnapshotChunk) (resp abcix.ResponseLoadSnapshotChunk) {
-	registerType("abci.RequestLoadSnapshotChunk", abci.RequestLoadSnapshotChunk{})
 	adaptGeneralization(&req, &resp, "abci.RequestLoadSnapshotChunk", app.abciApp.LoadSnapshotChunk)
 	return
 }
 
 func (app *adaptedApp) ApplySnapshotChunk(req abcix.RequestApplySnapshotChunk) (resp abcix.ResponseApplySnapshotChunk) {
-	registerType("abci.RequestApplySnapshotChunk", abci.RequestApplySnapshotChunk{})
 	adaptGeneralization(&req, &resp, "abci.RequestApplySnapshotChunk", app.abciApp.ApplySnapshotChunk)
 	return
 }
