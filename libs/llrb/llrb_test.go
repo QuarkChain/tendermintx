36397996f5e3acc4fb567c2cbd78d57fc9347dd8
pichao <pichao@quarkchain.org>
2020-08-19 16:25:39 -0700

    test

--- libs/llrb/llrb_test.go
+++ libs/llrb/llrb_test.go
@@ -3,27 +3,42 @@ package llrb
 import (
 	"bytes"
 	cr "crypto/rand"
-	"math/rand"
 	"testing"
 	"time"
 )
 
-func getNodeKeys(count int) []nodeKey {
-	perm := rand.Perm(count)
+func getNodeKeys(priorities []uint64) []nodeKey {
 	var nks []nodeKey
-	for i := 0; i < count; i++ {
-		nk := NewNodeKey(uint64(perm[i]), time.Now())
+	for i := 0; i < len(priorities); i++ {
+		nk := NewNodeKey(priorities[i], time.Now())
 		nks = append(nks, *(nk.(*nodeKey)))
 	}
 	return nks
 }
 
-func TestCases(t *testing.T) {
+func getRandomBytes(count int) [][]byte {
+	var txs [][]byte
+	for i := 0; i < count; i++ {
+		tx := make([]byte, 20)
+		cr.Read(tx)
+		txs = append(txs, tx)
+	}
+	return txs
+}
+
+func getOrderedTxs(tree *llrb) [][]byte {
+	var starter *nodeKey
+	var ordered
+	for result, err := tree.GetNext(starter, func(interface{}) bool { return true }); result != nil && err == nil; {
+
+	}
+}
+
+func TestBasics(t *testing.T) {
 	tree := NewLLRB()
-	nks := getNodeKeys(1)
-	txBytes := make([]byte, 20)
-	cr.Read(txBytes)
-	tree.Insert(nks[0].priority, nks[0].ts, txBytes)
+	nks := getNodeKeys([]uint64{1})
+	txs := getRandomBytes(1)
+	tree.Insert(nks[0].priority, nks[0].ts, txs[0])
 	if tree.Size() != 1 {
 		t.Errorf("expecting len 1")
 	}
@@ -31,7 +46,28 @@ func TestCases(t *testing.T) {
 	if tree.Size() != 0 {
 		t.Errorf("expecting len 0")
 	}
-	if !bytes.Equal(data.([]byte), txBytes) {
+	if !bytes.Equal(data.([]byte), txs[0]) {
 		t.Errorf("expecting equal bytes")
+	}
+}
+
+func TestGetNext(t *testing.T) {
+	testCases := []struct {
+		priorities []uint64
+		order      []int64
+	}{
+		// error case by wrong gas/bytes limit
+		{
+			priorities: []uint64{0, 0, 0, 0, 0},
+			order:      []int64{0, 1, 2, 3, 4},
+		},
+	}
+	for i, tc := range testCases {
+		tree := NewLLRB()
+		nks := getNodeKeys(tc.priorities)
+		txs := getRandomBytes(len(tc.priorities))
+		for j := 0; j < len(nks); j++ {
+			tree.Insert(nks[j].priority, nks[j].ts, txs[j])
+		}
 	}
 }
