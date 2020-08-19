22aa6a9345f2fb14dd48e4a2399adf75333c24a7
PiChao <66969090+pichaoqkc@users.noreply.github.com>
2020-08-19 16:23:39 -0700

    [abcix]llrb interface design (#44)

---
+++ libs/llrb/interface.go
@@ -0,0 +1,19 @@
+package llrb
+
+import "time"
+
+type NodeKey struct {
+	Priority uint64
+	TS       time.Time
+}
+
+type LLRB interface {
+	Size() int
+	GetNext(starter *NodeKey, predicate func(interface{}) bool) (interface{}, error)
+	Insert(starter NodeKey, data interface{}) error
+	Remove(starter NodeKey) (interface{}, error)
+}
+
+func New() LLRB {
+	panic("Not implemented")
+}
