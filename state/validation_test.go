ce168dee03913b5d3c971681444c3f405c0baba0
hanyunx <hanyunx2@gmail.com>
2020-08-26 12:51:43 -0700

    Remove LastResultsHash test case

--- state/validation_test.go
+++ state/validation_test.go
@@ -67,7 +67,6 @@ func TestValidateBlockHeader(t *testing.T) {
 		{"NextValidatorsHash wrong", func(block *types.Block) { block.NextValidatorsHash = wrongHash }},
 		{"ConsensusHash wrong", func(block *types.Block) { block.ConsensusHash = wrongHash }},
 		{"AppHash wrong", func(block *types.Block) { block.AppHash = wrongHash }},
-		{"LastResultsHash wrong", func(block *types.Block) { block.LastResultsHash = wrongHash }},
 
 		{"EvidenceHash wrong", func(block *types.Block) { block.EvidenceHash = wrongHash }},
 		{"Proposer wrong", func(block *types.Block) { block.ProposerAddress = ed25519.GenPrivKey().PubKey().Address() }},
