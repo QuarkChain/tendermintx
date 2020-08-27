fd46e16362ed2c1e50f9fba2acd2bbc52d785605
pichao <pichao@quarkchain.org>
2020-08-27 14:15:42 -0700

    add removeTx,GetNext and CheckTx benchmark

--- mempool/bench_test.go
+++ mempool/bench_test.go
@@ -1,17 +1,20 @@
 package mempool
 
 import (
+	"crypto/rand"
 	"encoding/binary"
-	"math/rand"
+	"math/big"
 	"strconv"
 	"testing"
 
 	"github.com/tendermint/tendermint/abci/example/kvstore"
 	cfg "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/proxy"
+	"github.com/tendermint/tendermint/types"
 )
 
-func BenchmarkCheckTx(b *testing.B) {
+// BenchmarkClistCheckTx-8   	10842198	        94.9 ns/op
+func BenchmarkClistCheckTx(b *testing.B) {
 	app := kvstore.NewApplication()
 	cc := proxy.NewLegacyLocalClientCreator(app)
 	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enumclistmempool)
@@ -21,6 +24,52 @@ func BenchmarkCheckTx(b *testing.B) {
 		tx := make([]byte, 8)
 		binary.BigEndian.PutUint64(tx, uint64(i))
 		mempool.CheckTx(tx, nil, TxInfo{})
+	}
+}
+
+// BenchmarkLLRBCheckTx-8   	10889796	        99.2 ns/op
+func BenchmarkLLRBCheckTx(b *testing.B) {
+	app := kvstore.NewApplication()
+	cc := proxy.NewLegacyLocalClientCreator(app)
+	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enumllrbmempool)
+	defer cleanup()
+
+	for i := 0; i < b.N; i++ {
+		tx := make([]byte, 8)
+		binary.BigEndian.PutUint64(tx, uint64(i))
+		mempool.CheckTx(tx, nil, TxInfo{})
+	}
+}
+
+// BenchmarkClistRemoveTx-8   	  480782	      2151 ns/op
+func BenchmarkClistRemoveTx(b *testing.B) {
+	benchmarkRemoveTx(b, enumclistmempool)
+}
+
+// BenchmarkLLRBRemoveTx-8   	  541332	      2115 ns/op
+func BenchmarkLLRBRemoveTx(b *testing.B) {
+	benchmarkRemoveTx(b, enumllrbmempool)
+}
+
+func benchmarkRemoveTx(b *testing.B, enum mpEnum) {
+	app := kvstore.NewApplication()
+	cc := proxy.NewLegacyLocalClientCreator(app)
+	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enum)
+	defer cleanup()
+	size := 100000
+
+	txs := types.Txs{}
+	for j := 0; j < size; j++ {
+		rand, _ := rand.Int(rand.Reader, big.NewInt(100))
+		tx := "k" + strconv.Itoa(j) + "=v" + strconv.Itoa(j) + ",f," + strconv.FormatInt(rand.Int64(), 10)
+		txBytes := []byte(tx)
+		txs = append(txs, txBytes)
+		mempool.CheckTx(txBytes, nil, TxInfo{})
+	}
+
+	for i := 0; i < b.N; i++ {
+		testmempool := mempool
+		testmempool.RemoveTxs(txs)
 	}
 }
 
@@ -53,36 +102,25 @@ func BenchmarkCacheRemoveTime(b *testing.B) {
 	}
 }
 
-// BenchmarkCListMempool_GetNextTxBytes-8   	       1	1099547205 ns/op
-func BenchmarkCListMempool_GetNextTxBytes(b *testing.B) {
-	app := kvstore.NewApplication()
-	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), 0)
-	defer cleanup()
-	size := 100000
-	for i := 0; i < size; i++ {
-		tx := "k" + strconv.Itoa(i) + "=v" + strconv.Itoa(i) + ",f," + strconv.FormatInt(int64(rand.Int())%100, 10)
-		txBytes := []byte(tx)
-		mempool.CheckTx(txBytes, nil, TxInfo{})
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		starter, _ := mempool.GetNextTxBytes(1000, 1, nil)
-		for starter != nil {
-			starter, _ = mempool.GetNextTxBytes(1000, 1, starter)
-		}
-	}
+// BenchmarkClistMempoolGetNextTxBytes-8   	       2	 793475198 ns/op
+func BenchmarkClistMempoolGetNextTxBytes(b *testing.B) {
+	benchmarkMempoolGetNextTxBytes(b, enumclistmempool)
 }
 
-// BenchmarkLlrbMempool_GetNextTxBytes-8   	     250	   5048917 ns/op
-func BenchmarkLlrbMempool_GetNextTxBytes(b *testing.B) {
+// BenchmarkLLRBMempoolGetNextTxBytes-8   	     322	   3565640 ns/op
+func BenchmarkLLRBMempoolGetNextTxBytes(b *testing.B) {
+	benchmarkMempoolGetNextTxBytes(b, enumllrbmempool)
+}
+
+func benchmarkMempoolGetNextTxBytes(b *testing.B, enum mpEnum) {
 	app := kvstore.NewApplication()
 	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), 1)
+	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enum)
 	defer cleanup()
 	size := 100000
 	for i := 0; i < size; i++ {
-		tx := "k" + strconv.Itoa(i) + "=v" + strconv.Itoa(i) + ",f," + strconv.FormatInt(int64(rand.Int())%100, 10)
+		rand, _ := rand.Int(rand.Reader, big.NewInt(100))
+		tx := "k" + strconv.Itoa(i) + "=v" + strconv.Itoa(i) + ",f," + strconv.FormatInt(rand.Int64(), 10)
 		txBytes := []byte(tx)
 		mempool.CheckTx(txBytes, nil, TxInfo{})
 	}
