c3f16a0eeb85e086540321f997326fcb116707c3
pichao <pichao@quarkchain.org>
2020-08-28 02:36:07 -0700

    use abcix kvstore

--- mempool/bench_test.go
+++ mempool/bench_test.go
@@ -3,56 +3,69 @@ package mempool
 import (
 	"crypto/rand"
 	"encoding/binary"
+	"fmt"
 	"math/big"
 	"strconv"
+	"strings"
 	"testing"
 
-	"github.com/tendermint/tendermint/abci/example/kvstore"
+	"github.com/tendermint/tendermint/abcix/example/kvstore"
 	cfg "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/proxy"
 	"github.com/tendermint/tendermint/types"
 )
 
-// BenchmarkClistCheckTx-8   	10842198	        94.9 ns/op
+// BenchmarkClistCheckTx-8   	     768	   1510694 ns/op
 func BenchmarkClistCheckTx(b *testing.B) {
-	app := kvstore.NewApplication()
-	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enumclistmempool)
-	defer cleanup()
-	for i := 0; i < b.N; i++ {
-		tx := make([]byte, 8)
-		binary.BigEndian.PutUint64(tx, uint64(i))
-		mempool.CheckTx(tx, nil, TxInfo{})
-	}
+	benchmarkCheckTx(b, enumclistmempool)
 }
 
-// BenchmarkLLRBCheckTx-8   	10889796	        99.2 ns/op
+// BenchmarkLLRBCheckTx-8   	     646	   1573239 ns/op
 func BenchmarkLLRBCheckTx(b *testing.B) {
+	benchmarkCheckTx(b, enumllrbmempool)
+}
+
+func benchmarkCheckTx(b *testing.B, enum mpEnum) {
 	app := kvstore.NewApplication()
-	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enumllrbmempool)
+	cc := proxy.NewLocalClientCreator(app)
+	mempool, cleanup := newMempoolWithAppAndConfig(cc, cfg.ResetTestRoot(fmt.Sprintf("mempool_test_%d", enum)), enum)
 	defer cleanup()
+	size := 100
+	txs := types.Txs{}
+	for i := 0; i < size; i++ {
+		txBytes := make([]byte, 20)
+		priority := strconv.FormatInt(int64(i)%100, 10)
+		tx := "k" + strconv.Itoa(i) + "=v" + strconv.Itoa(i) + ","
+		extra := 20 - len(tx) - len(priority) - 1
+		tx = tx + strings.Repeat("f", extra) + "," + priority
+		copy(txBytes, tx)
+		txs = append(txs, txBytes)
+	}
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		tx := make([]byte, 8)
-		binary.BigEndian.PutUint64(tx, uint64(i))
-		mempool.CheckTx(tx, nil, TxInfo{})
+		for j := 0; j < size; j++ {
+			mempool.CheckTx(txs[j], nil, TxInfo{})
+		}
+		b.StopTimer()
+		mempool.Flush()
+		b.StartTimer()
 	}
 }
 
-// BenchmarkClistRemoveTx-8   	  749619	      1625 ns/op
+// BenchmarkClistRemoveTx-8   	  731764	      1619 ns/op
 func BenchmarkClistRemoveTx(b *testing.B) {
 	benchmarkRemoveTx(b, enumclistmempool)
 }
 
-// BenchmarkLLRBRemoveTx-8   	  724825	      1609 ns/op
+// BenchmarkLLRBRemoveTx-8   	  726847	      1642 ns/op
 func BenchmarkLLRBRemoveTx(b *testing.B) {
 	benchmarkRemoveTx(b, enumllrbmempool)
 }
 
 func benchmarkRemoveTx(b *testing.B, enum mpEnum) {
 	app := kvstore.NewApplication()
-	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enum)
+	cc := proxy.NewLocalClientCreator(app)
+	mempool, cleanup := newMempoolWithAppAndConfig(cc, cfg.ResetTestRoot(fmt.Sprintf("mempool_test_%d", enum)), enum)
 	defer cleanup()
 	size := 100000
 	txs := types.Txs{}
@@ -99,20 +112,20 @@ func BenchmarkCacheRemoveTime(b *testing.B) {
 	}
 }
 
-// BenchmarkClistMempoolGetNextTxBytes-8   	       2	 793475198 ns/op
+// BenchmarkClistMempoolGetNextTxBytes-8   	       2	 605761241 ns/op
 func BenchmarkClistMempoolGetNextTxBytes(b *testing.B) {
 	benchmarkMempoolGetNextTxBytes(b, enumclistmempool)
 }
 
-// BenchmarkLLRBMempoolGetNextTxBytes-8   	     322	   3565640 ns/op
+// BenchmarkLLRBMempoolGetNextTxBytes-8   	     324	   3757553 ns/op
 func BenchmarkLLRBMempoolGetNextTxBytes(b *testing.B) {
 	benchmarkMempoolGetNextTxBytes(b, enumllrbmempool)
 }
 
 func benchmarkMempoolGetNextTxBytes(b *testing.B, enum mpEnum) {
 	app := kvstore.NewApplication()
-	cc := proxy.NewLegacyLocalClientCreator(app)
-	mempool, cleanup := newLegacyMempoolWithAppAndConfig(cc, cfg.ResetTestRoot("mempool_test"), enum)
+	cc := proxy.NewLocalClientCreator(app)
+	mempool, cleanup := newMempoolWithAppAndConfig(cc, cfg.ResetTestRoot(fmt.Sprintf("mempool_test_%d", enum)), enum)
 	defer cleanup()
 	size := 100000
 	for i := 0; i < size; i++ {
